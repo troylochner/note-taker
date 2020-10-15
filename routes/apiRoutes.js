0c1d46db6d6c9e4fa8d0e36cd21ed815acb207c2
troylochner <troy.lochner@gmail.com>
2020-10-15 18:42:43 -0400

    Updated delete process

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const util = require("util");
 const uuid = require("uuid");
 const dayjs = require("dayjs");
-var notes = require("../db/db");
+var notes = require("../db/db.json");
 
 // ===============================================================================
 // ROUTING
@@ -15,23 +15,26 @@ module.exports = function(app) {
 // API GET NOTES
 // ---------------------------------------------------------------------------
   app.get("/api/notes/", function(req,res){
-    return res.json(notes);
+    res.json(notes);
 }) ; 
 
 // API POST NOTE
 // ---------------------------------------------------------------------------
   app.post("/api/notes", function (req, res) {
     var addNote = req.body;
+    
+    //ADDING UUID + TIMESTAMP TO EACH NOTE
     addNote.uuid = uuid.v4()
     addNote.timestamp = dayjs().format();
+
+    //ADD TO THE NOTE ARRAY
     notes.push(addNote);
     res.json(addNote);
-      
-    fs.readFile('db/db.json', 'utf8', function (err, data) {
-     fs.writeFile('db/db.json', JSON.stringify(notes), function(err, result) {
-        if(err) console.log('error', err);
-      });
-    });
+    
+    //WRITE THE ADDED NOTE TO THE FILE
+    fs.writeFileSync('db/db.json', JSON.stringify(notes), function(err, result) {
+      if(err) console.log('error', err);
+    }); 
 });
 
 
@@ -39,9 +42,21 @@ module.exports = function(app) {
 // ---------------------------------------------------------------------------
 app.delete("/api/notes/:uuid", function(req,res){
   var uuid = req.params.uuid ;
-  console.log(`Deleting Note : ${uuid}`)
-  let obj = notes.find(obj => obj.uuid == uuid);
-  res.json(obj)
+  console.log(`Deleting Note : ${uuid}`);
+  
+  const removedNote = notes.find(removedNote => removedNote.uuid == uuid);
+  
+//REMOVE THE NOTE FROM ARRAY
+  const updatedNotes = notes.filter(notes => notes.uuid != uuid);
+
+//WRITE THE UPDATED NOTES TO OUR FILE
+
+  fs.writeFileSync('db/db.json', JSON.stringify(updatedNotes), function(err, result) {
+     if(err) console.log('error', err);
+   });
+
+   //RETURN THE REMOVED NOTE TO THE USER  
+  res.json(removedNote)
 
 })
 
