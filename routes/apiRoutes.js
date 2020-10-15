a6cbc515d1db4faac6200c2ab0de9d75d6a3fbc9
troylochner <troy.lochner@gmail.com>
2020-10-15 17:29:20 -0400

    updates to the api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,82 +1,45 @@
 // ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// LOAD DATA & DEPANDANCIES
 // ===============================================================================
 const fs = require("fs");
 const util = require("util");
 const uuid = require("uuid");
 const dayjs = require("dayjs");
-//var unreadRequest = require("../data/unreadRequest");
+var notes = require("../db/db");
 
-
-// ================================================================================
-//GET UNREAD REQUESTS
-
-/*
-var unreadRequest = [] ; 
-
-fs.readFile("data/unreadRequest.json", "utf8", function(error, data) {
-    if (error) {
-      return ;
-    }
-    unreadRequest= JSON.parse(data);
-  });*/
-  // ================================================================================
-
-
-
-/*
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
-*/
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+  
+// API GET NOTES
+// ---------------------------------------------------------------------------
   app.get("/api/notes/", function(req,res){
     return res.json(notes);
 }) ; 
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+// API POST NOTE
+// ---------------------------------------------------------------------------
   app.post("/api/notes", function (req, res) {
     var addNote = req.body;
-    //addNote.uuid = uuid.v4()
-    //addNote.timestamp = dayjs().format();
+    addNote.uuid = uuid.v4()
+    addNote.timestamp = dayjs().format();
+    notes.push(addNote);
     res.json(addNote);
-    //console.log(newRequest);
-    //fs.writeFile("data/unreadRequest.json", JSON.stringify(unreadRequest))
-    //writeFileAsync("data/unreadRequest.json", JSON.stringify(unreadRequest))
+      
+    fs.readFile('db/db.json', 'utf8', function (err, data) {
+     fs.writeFile('db/db.json', JSON.stringify(notes), function(err, result) {
+        if(err) console.log('error', err);
+      });
+    });
 });
 
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-/*
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
-  });
-};
-
-*/
+// API DELETE NOTE
+// ---------------------------------------------------------------------------
+app.delete("/api/notes/:uuid", function(req,res){
+    var uuid = req.param.uuid
+    res.json()
+})
 
 }
 
